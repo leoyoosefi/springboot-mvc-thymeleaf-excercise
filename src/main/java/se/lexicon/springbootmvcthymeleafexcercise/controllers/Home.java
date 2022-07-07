359d19362326cad5e99bb223132241e74c37a3d8
Leo Yoosefi <101973222+leoyoosefi@users.noreply.github.com>
2022-07-07 12:13:24 +0200

    exercise 2(contact.html)

--- src/main/java/se/lexicon/springbootmvcthymeleafexcercise/controllers/Home.java
+++ src/main/java/se/lexicon/springbootmvcthymeleafexcercise/controllers/Home.java
@@ -1,13 +1,30 @@
 package se.lexicon.springbootmvcthymeleafexcercise.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.List;
 
 @Controller
 public class Home {
 
-@GetMapping("/index")
-    public String index(){
+    @GetMapping("/index")
+    public String index() {
         return "index";
     }
+
+    @GetMapping("/contact")
+    public String contact() {
+        return "contact";
+    }
+
+    @PostMapping("/contact")
+    public String contact(@RequestParam List<String>contact) {
+        return "contact";
+
+    }
+
 }
