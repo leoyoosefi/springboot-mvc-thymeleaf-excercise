53a6ac61e6490d5b876643f1108a6583978842ce
Leo Yoosefi <leoyoosefi@gmail.com>
2022-07-08 10:04:18 +0200

    Home controller complete

--- src/main/java/se/lexicon/springbootmvcthymeleafexcercise/controllers/Home.java
+++ src/main/java/se/lexicon/springbootmvcthymeleafexcercise/controllers/Home.java
@@ -2,30 +2,48 @@ package se.lexicon.springbootmvcthymeleafexcercise.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Controller
-public class Home {
+public class Home
+{
+    private List<String> contactList = new ArrayList<>();
 
-    @GetMapping("/index")
-    public String index() {
+    public Home() {
+        contactList.add("Leo Yoosefi: 070-***8723");
+    }
+
+    @GetMapping("/home")
+    public String home(){
         return "index";
     }
 
+    @GetMapping("/index")
+    public String index(){
+        return "index";
+    }
 
     @GetMapping("/contact")
-    public String contact() {
+    public String contact(){
         return "contact";
     }
 
     @PostMapping("/contact")
-    public String contact(@RequestParam List<String>contact) {
-        return "contact";
+    public String contact(@RequestParam String contactInput){
 
+        contactList.add(contactInput);
+        return "contact";
+    }
+
+    @GetMapping("/list")
+    public String contactList(Model model)
+    {
+        model.addAttribute("Contacts",contactList);
+
+        return "contact";
     }
 
 }
