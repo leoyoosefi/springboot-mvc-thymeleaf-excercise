91e0f3d4165f2c3d731d1f0541ad3d31f5152092
Leo Yoosefi <leoyoosefi@gmail.com>
2022-07-10 16:41:22 +0200

    Corrected home controller

--- src/main/java/se/lexicon/springbootmvcthymeleafexcercise/controllers/Home.java
+++ src/main/java/se/lexicon/springbootmvcthymeleafexcercise/controllers/Home.java
@@ -13,37 +13,53 @@ public class Home
     private List<String> contactList = new ArrayList<>();
 
     public Home() {
+        System.out.println("default data");
         contactList.add("Leo Yoosefi: 070-***8723");
+        contactList.add("Mehrdad Javan: 070-***9898");
+        contactList.add("Anna Andersson: 070-***8433");
     }
+
+    @RequestMapping(value = "/" , method = RequestMethod.GET)
+    public String defaultURL() {
+        System.out.println("##### default method executed ####");
+        return "index";
+    }
+
 
     @GetMapping("/home")
     public String home(){
+        System.out.println("##### home method executed ####");
         return "index";
     }
 
     @GetMapping("/index")
     public String index(){
+        System.out.println("##### index method executed ####");
         return "index";
     }
 
     @GetMapping("/contact")
-    public String contact(){
+    public String displayContactForm(){
+        System.out.println("##### contact method executed ####");
         return "contact";
     }
 
     @PostMapping("/contact")
-    public String contact(@RequestParam String contactInput){
+    public String contact(@RequestParam("contactInfo") String contact){
+        System.out.println("Contact Info:" + contact);
 
-        contactList.add(contactInput);
-        return "contact";
+
+        contactList.add(contact);
+        return "contactInput";
     }
 
-    @GetMapping("/list")
+    @GetMapping("/contactList")
     public String contactList(Model model)
     {
-        model.addAttribute("Contacts",contactList);
+        System.out.println("Number of Contacts: " + contactList.size());
+        model.addAttribute("ContactList",contactList);
 
-        return "contact";
+        return "contactList";
     }
 
 }
