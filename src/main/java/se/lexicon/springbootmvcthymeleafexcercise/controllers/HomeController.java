434e5f0830b0d2b972b645ac158535b5d19e2f4c
Leo Yoosefi <leoyoosefi@gmail.com>
2022-07-11 16:31:17 +0200

    controller & contact + contactList fixed

---
+++ src/main/java/se/lexicon/springbootmvcthymeleafexcercise/controllers/HomeController.java
@@ -0,0 +1,44 @@
+package se.lexicon.springbootmvcthymeleafexcercise.controllers;
+
+import org.hibernate.mapping.Array;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+@Controller
+public class HomeController
+{
+    List<String> contactList = new ArrayList<>();
+
+
+    @GetMapping({"/","/index"})
+    public String index(){
+        return "index";
+    }
+    @GetMapping("/contact")
+    public String contact(){
+        return "contact";
+}
+    @PostMapping("/contact")
+    public String contact(@RequestParam("email") String email){
+    System.out.println("Email: " + email);
+    contactList.add(email);
+    return "redirect:/contactList";
+
+}
+@GetMapping("/contactList")
+public String contactList(Model model){
+        model.addAttribute("contactList",contactList);
+        return "contactList";
+
+
+
+   }
+
+}
